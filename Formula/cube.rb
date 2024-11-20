7a163bbc73b3a181fbf6d67a614e99a621ae0173
aa830 <84448225+aa830@users.noreply.github.com>
2024-11-20 22:43:02 +0800

    fix

--- Formula/cube.rb
+++ Formula/cube.rb
@@ -1,9 +1,9 @@
 class Cube < Formula
     desc "A cute spinning cube for your terminal!"
     homepage "https://github.com/aa830/cube.c"
-    url "https://github.com/aa830/homebrew-cube.c/archive/refs/tags/v0.0.1.tar.gz"
-    sha256 "c894fc542af8e650d873601c2068f03c3a7c20add2e272b0a12a60726cbb6b8c"
-    version "v0.0.1"
+    url "https://github.com/aa830/homebrew-cube.c/archive/refs/tags/v0.0.2.tar.gz"
+    sha256 "1958be6889f302c52c46785c5839c714b65627eea653caa5dae6f7c472e62474"
+    version "v0.0.2"
   
     def install
       # Compile the project using `make`
